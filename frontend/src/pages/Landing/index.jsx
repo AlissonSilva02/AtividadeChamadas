4bf80ad9a8c50cf79024aedffa5d38d90bb66d1c
alisson <ra54079082800@acaonsfatima.org.br.com@example.com>
2024-11-19 01:05:29 -0300

    fazer a logica e o css

--- frontend/src/pages/Landing/index.jsx
+++ frontend/src/pages/Landing/index.jsx
@@ -4,26 +4,46 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import Cabecalho from "../../components/cabecalho";
 
-
 export default function App() {
-    const [filtro, setFiltro] = useState();
+  const [filtro, setFiltro] = useState();
 
+  function filtros() {}
 
-    function filtros() {
+  return (
+    <div className="landing">
+      <Cabecalho />
 
-    }
-
-    return (
-        <div className="landing">
-            <Cabecalho />
-
-            <div className="pesquisar">
-
-                <div className="lupa">
-                    <input type="text" placeholder="Filtrar..."  onChange={(e) => setFiltro((e.target.value))} />
-                    <button onClick={filtros}> <img src="/assets/images/lupa.png" alt="" /></button>
-                </div>
-            </div>
+      <div className="pesquisar">
+        <div className="lupa">
+          <input
+            type="text"
+            placeholder="Filtrar..."
+            onChange={(e) => setFiltro(e.target.value)}
+          />
+          <button onClick={filtros}>
+            {" "}
+            <img src="/assets/images/lupa.png" alt="" />
+          </button>
         </div>
-    )
+      </div>
+
+      <div className="tabela">
+        <div className="titulos">
+          <h1>Título</h1>
+          <h1>Impacto</h1>
+          <h1>Data Ocorrência</h1>
+          <h1>Atribuído</h1>
+        </div>
+
+        {/*fazer o .map para retornar os resultados*/}
+      </div>
+
+      <Link to={"/cadastrar"}>
+        <div className="cadastrar">
+          <h1>Novo chamado</h1>
+        </div>
+      </Link>
+      
+    </div>
+  );
 }
--- frontend/src/pages/cadastrar/index.jsx
+++ frontend/src/pages/cadastrar/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react"; // Mantenha apenas esta linha
+import React, { useState, useEffect } from "react"; // Mantenha apenas esta linha
 import "./index.scss";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -6,9 +6,51 @@ import Cabecalho from "../../components/cabecalho";
 
 export default function Cadastrar() {
 
-    return(
-        <div className="pagina-cadastrar">
-            <Cabecalho/>
+    function cadastrar(){
+
+    }
+  return (
+    <div className="pagina-cadastrar">
+      <Cabecalho />
+
+      <img src="/assets/images/negocios.png" alt="" />
+
+      <div className="formulario">
+        <h1 className="titulo">Detalhes do Chamado</h1>
+
+        <div className="informacoes">
+          <h2>Título</h2>
+          <input type="text" placeholder="Informe um título..." />
+
+          <h2>Informações</h2>
+          <input type="text" placeholder="### Insira as informações aqui..." />
+
+          <div className="opcoes">
+            <h2>Impacto</h2>
+            <input type="text" />
+
+            <h2>Data da Ocorrência</h2>
+            <input type="date" placeholder="01/01/2024" />
+
+            <h2>Atribuir</h2>
+            <input type="" placeholder="Selecione o usuário responsável..." />
+          </div>
         </div>
-    )
+
+        <Link to={-1}>
+          <div className="voltar">
+            <p>Voltar</p>
+          </div>
+        </Link>
+
+        <Link to={"/"}>
+                <div className="salvar">
+
+                    <button onClick={cadastrar}>Salvar</button>
+
+                </div>
+            </Link>
+      </div>
+    </div>
+  );
 }
